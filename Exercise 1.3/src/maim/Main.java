1fe8ae33301d900914a63bee50714ecb881f6dc1
leetzongying <73087882+leetzongying@users.noreply.github.com>
2021-04-01 14:44:41 +0800

    Update Main.java

--- Exercise 1.3/src/maim/Main.java
+++ Exercise 1.3/src/maim/Main.java
@@ -3,16 +3,47 @@ package maim;
 public class Main {
 
 	public static void main(String[] args) {
-	
-		Smartphone c = new Smartphone();
-		 System.out.println(c.model);
-		 System.out.println(c.OS);
-		 System.out.println(c.releasedyear);
-		 System.out.println(c.brand);
-		 c.notification();
-		 c.mute();
-		 c.screenshot();
-		 c.alarm();
+		Smartphone Huawei = new Smartphone();
+		 System.out.println("This is " + Huawei.model[0]);
+		 System.out.println("The brand is "+ Huawei.brand[0]);
+		 System.out.println("This phone is run in "+ Huawei.OS[0]);
+		 System.out.println("This phone is released in " + Huawei.releasedyear[0]);
+		 
+		 System.out.println("\n");
+		 
+		 Huawei.notification();
+		 Huawei.mute();
+		 Huawei.screenshot();
+		 Huawei.alarm();
+		 
+		 System.out.println("\n");
+		 
+		 Smartphone Samsung = new Smartphone();
+		 System.out.println("This is " + Samsung.model[1]);
+		 System.out.println("The brand is "+ Samsung.brand[1]);
+		 System.out.println("This phone is run in "+ Samsung.OS[1]);
+		 System.out.println("This phone is released in " + Samsung.releasedyear[1]);
+		 
+		 System.out.println("\n");
+		 
+		 Samsung.notification();
+		 Samsung.mute();
+		 Samsung.screenshot();
+		 Samsung.alarm();
+		 
+		 System.out.println("\n");
+		 
+		 Smartphone Apple = new Smartphone();
+		 System.out.println("This is " + Apple.model[1]);
+		 System.out.println("The brand is "+ Apple.brand[1]);
+		 System.out.println("This phone is run in "+ Apple.OS[1]);
+		 System.out.println("This phone is released in " + Apple.releasedyear[1]);
+		 System.out.println("\n");
+		 Apple.notification();
+		 Apple.mute();
+		 Apple.screenshot();
+		 Apple.alarm();
+		
 
 	}
 
