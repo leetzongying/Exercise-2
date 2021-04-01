8d65b46837ef0727235a0bbd4ea7e2143d847f2f
leetzongying <73087882+leetzongying@users.noreply.github.com>
2021-04-01 14:45:13 +0800

    Update Smartphone.java

--- Exercise 1.3/src/maim/Smartphone.java
+++ Exercise 1.3/src/maim/Smartphone.java
@@ -2,10 +2,10 @@ package maim;
 
 public class Smartphone {
 	
-	String model = "Huawei P30"; 
-	String OS = "Android 9.0 \"Pie\"";
-	int releasedyear = 2019;
-	String brand = "Huawei";
+	String model []= {"Huawei P30", "Samsung Galaxy S21", "iPhone 12 Pro"}; 
+	String OS [] = {"Android 9.0 \"Pie\"", "One UI 3.1, Android 11","iOS 14.1"};
+	int releasedyear [] = {2019, 2021, 2020};
+	String brand[] = {"Huawei", "Samsung","Apple"};
 
 	void notification() {
 		System.out.println("You have 99+ unread messages.");
