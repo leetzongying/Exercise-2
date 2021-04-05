607a03bbfb47245a3cd6a187406dbff8a2691bb5
leetzongying <73087882+leetzongying@users.noreply.github.com>
2021-04-05 16:35:25 +0800

    Update Main.java
    Exercise 2.2

--- Exercise 1.3/src/maim/Main.java
+++ Exercise 1.3/src/maim/Main.java
@@ -4,45 +4,51 @@ public class Main {
 
 	public static void main(String[] args) {
 		Smartphone Huawei = new Smartphone();
-		 System.out.println("This is " + Huawei.model[0]);
-		 System.out.println("The brand is "+ Huawei.brand[0]);
-		 System.out.println("This phone is run in "+ Huawei.OS[0]);
-		 System.out.println("This phone is released in " + Huawei.releasedyear[0]);
+		Smartphone Samsung = new Smartphone();
+		Smartphone Apple = new Smartphone();
+		Smartphone Nokia = new Smartphone();
+		
+		System.out.println("Phone 1 Specifications");
+		Huawei.printSpec("P30","Huawei","Android 9.0 \"Pie\"",2019);
+		 
+		System.out.println("\n");
+		 
+		 Huawei.printTotal(599, 3, "P30");
+		 Huawei.weight("P30",1.1,3);
+		 Huawei.alarm(7,25,40);
 		 
 		 System.out.println("\n");
 		 
-		 Huawei.notification();
-		 Huawei.mute();
-		 Huawei.screenshot();
-		 Huawei.alarm();
+		 System.out.println("Phone 2 Specifications");
+		Samsung.printSpec("Samsung Galaxy S21","Samsung","One UI 3.1, Android 11",2021);
 		 
 		 System.out.println("\n");
 		 
-		 Smartphone Samsung = new Smartphone();
-		 System.out.println("This is " + Samsung.model[1]);
-		 System.out.println("The brand is "+ Samsung.brand[1]);
-		 System.out.println("This phone is run in "+ Samsung.OS[1]);
-		 System.out.println("This phone is released in " + Samsung.releasedyear[1]);
+		 Samsung.printTotal(699, 3, "Galaxy S21");
+		 Samsung.weight("Galaxy S21",0.8, 3);
+		 Samsung.alarm(8,30,40);
 		 
 		 System.out.println("\n");
 		 
-		 Samsung.notification();
-		 Samsung.mute();
-		 Samsung.screenshot();
-		 Samsung.alarm();
+		 System.out.println("Phone 3 Specifications");
+		 Apple.printSpec("iPhone 12 Pro","Apple","iOS 14.1",2020);
 		 
 		 System.out.println("\n");
 		 
-		 Smartphone Apple = new Smartphone();
-		 System.out.println("This is " + Apple.model[1]);
-		 System.out.println("The brand is "+ Apple.brand[1]);
-		 System.out.println("This phone is run in "+ Apple.OS[1]);
-		 System.out.println("This phone is released in " + Apple.releasedyear[1]);
+		 Apple.printTotal(569, 3, "iPhone 12 Pro");
+		 Apple.weight("iPhone 12 Pro",1.2, 3);
+		 Apple.alarm(9,10,25);
+		 
+         System.out.println("\n");
+		 
+		 System.out.println("Phone 4 Specifications");
+		 Nokia.printSpec("Nokia 3310","Nokia","Yun OS",2020);
+		 
 		 System.out.println("\n");
-		 Apple.notification();
-		 Apple.mute();
-		 Apple.screenshot();
-		 Apple.alarm();
+		 
+		 Nokia.printTotal(569, 3, "Nokia 3310");
+		 Nokia.weight("Nokia 3310",1.5, 3);
+		 Nokia.alarm(6,17,58);
 		
 
 	}
