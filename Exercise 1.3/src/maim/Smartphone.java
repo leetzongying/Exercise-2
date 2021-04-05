29df4ef246fd1e1c8fe3aa9e39c8a97c8302bcd4
leetzongying <73087882+leetzongying@users.noreply.github.com>
2021-04-05 16:36:06 +0800

    Update Smartphone.java
    Exercise 2.2

--- Exercise 1.3/src/maim/Smartphone.java
+++ Exercise 1.3/src/maim/Smartphone.java
@@ -2,27 +2,36 @@ package maim;
 
 public class Smartphone {
 	
-	String model []= {"Huawei P30", "Samsung Galaxy S21", "iPhone 12 Pro"}; 
-	String OS [] = {"Android 9.0 \"Pie\"", "One UI 3.1, Android 11","iOS 14.1"};
-	int releasedyear [] = {2019, 2021, 2020};
-	String brand[] = {"Huawei", "Samsung","Apple"};
+	String model;
+	String OS ;
+	int releasedyear;
+	String brand;
+	double totalPrice, totalWeight;
 
-	void notification() {
-		System.out.println("You have 99+ unread messages.");
+	void printSpec(String model, String brand, String OS, int releasedyear) {
+		System.out.println("Model: " + model);
+		System.out.println("Brand: " + brand);
+		System.out.println("Operating System: " + OS);
+		System.out.println("Released Year: " + releasedyear);
 	}
 	
-	void mute() {
-		boolean muted = true;
-		System.out.println("Status of mute: " + muted);
+	void printTotal(int p, int q, String b){
+        totalPrice = (p*q);
+        
+        System.out.println("The Unit Price of "+b+" is : RM "+p);
+        System.out.println("The Total Unit Purchased is : " + q + " units.");
+        System.out.println("The Total Price for "+q+" units of "+b+" is : RM"+totalPrice);
+    }
+	
+	void weight(String brand, double weight, int quantity) {
+		totalWeight = (quantity*weight);
+		
+		System.out.println("The Unit Weight of " + brand + " is :" + weight +"kg");
+		System.out.println("The Total Unit Purchased is : " + quantity + " units.");
+		System.out.println("The total weight for " + quantity + " " + brand + " is :" + totalWeight + "kg");
 	}
 	
-	void screenshot() {
-		boolean button1 = true;
-		System.out.println("Your screen has been captured: " + button1);
-	}
-	
-	void alarm() {
-		boolean setalarm = true;
-		System.out.println("Your alarm time has been seted: " + setalarm);
+	void alarm(int hour, int minute, int second) {
+		System.out.println("Your alarm time has been set to " + hour + ":" + minute + ":" + second);
 	}
 }
